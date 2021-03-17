3567848c81c8b5641a1f8991f250b8d1234c3805
cristhian urrego <infoafuro@gmail.com>
2021-03-17 11:34:32 -0500

    create new window

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const { Menu } = require('electron');
 const { app, BrowserWindow } = electron;
 
 let mainWindow;
+let addWindow;
 
 // Listen for app ready
 app.on('ready', function() {
@@ -23,11 +24,40 @@ app.on('ready', function() {
   Menu.setApplicationMenu(mainMenu);
 });
 
+// Handle create add window
+function createAddWindow() {
+  // create new window
+  addWindow = new BrowserWindow({
+    height: 200,
+    width: 300,
+    title: 'Add new item',
+    alwaysOnTop: true,
+    minimizable: false,
+    maximizable: false,
+    resizable: false,
+  });
+
+  // load html into window
+  addWindow.loadURL(`file://${__dirname}/addWindow.html`);
+
+  // garbage collection handle
+  addWindow.on('close', function() {
+    addWindow = null;
+  });
+}
+
 // create menu template
 const mainMenuTemplate = [
   {
     label: 'File',
     submenu: [
+      {
+        label: 'Add item',
+        accelerator: process.platform == 'darwin' ? 'Command+N' : 'Ctrl+N',
+        click() {
+          createAddWindow();
+        }
+      },
       {
         label: 'Quit',
         accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
